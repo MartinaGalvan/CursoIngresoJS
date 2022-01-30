567dc3267595c60392edd99da9a4c19050a8d424
MartinaGalvan <martinaiaragalvan@gmail.com>
2022-01-30 18:15:55 -0300

    nuevo cambio

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,8 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("ok");
+	alert("ok hola mundoo");
+
+	alert("hola de nuevo");
 }
 
